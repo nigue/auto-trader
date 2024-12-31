eaaef4fb032451649dcd16d3246ef079f95a1321
nigue <anibaldavor@gmail.com>
2024-12-30 23:39:15 -0300

    feat: update time in site

--- src/main/java/com/tapir/goose/view/SiteView.java
+++ src/main/java/com/tapir/goose/view/SiteView.java
@@ -1,21 +1,28 @@
 package com.tapir.goose.view;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Named("site")
-@SessionScoped
+@RequestScoped
 public class SiteView implements Serializable {
 
     private static final Logger logger = LogManager.getLogger(SiteView.class);
     
     private Boolean active;
     private String key;
+    private String date;
 
     @PostConstruct
     public void init() {
@@ -23,6 +30,9 @@ public class SiteView implements Serializable {
                 .getExternalContext()
                 .getFlash()
                 .get("key");
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        date = dateFormat.format(Calendar.getInstance().getTime());
         logger.info("Navigate to site with key: {}", key);
     }
 
@@ -33,7 +43,12 @@ public class SiteView implements Serializable {
     public void setKey(String key) {
         this.key = key;
     }
-    
-    
-    
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
