8099e5e77bfce1155c8acafb248ac61aa8979ed1
nigue <anibaldavor@gmail.com>
2025-01-01 19:11:57 -0300

    not merge: testing update state login

--- src/main/java/com/tapir/goose/view/IndexView.java
+++ src/main/java/com/tapir/goose/view/IndexView.java
@@ -6,6 +6,8 @@ import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,10 +19,13 @@ public class IndexView implements Serializable {
     
     private Boolean active;
     private String key;
+    private Integer step;
+    private Boolean running = false; // Para controlar la simulación
 
     @PostConstruct
     public void init() {
         active = false;
+        step = 0;
     }
     
     public Boolean getActive() {
@@ -39,6 +44,22 @@ public class IndexView implements Serializable {
         this.key = key;
     }
 
+    public Integer getStep() {
+        return step;
+    }
+
+    public void setStep(Integer step) {
+        this.step = step;
+    }
+
+    public Boolean getRunning() {
+        return running;
+    }
+
+    public void setRunning(Boolean running) {
+        this.running = running;
+    }
+
     public String login() {
         logger.info("Login at {}", new Date());
         logger.info("Login with key: {}", key);
@@ -47,7 +68,27 @@ public class IndexView implements Serializable {
                 .getExternalContext()
                 .getFlash()
                 .put("key", key);
-        
+
+
         return "site";
+    }
+
+    public void enter() throws InterruptedException {
+        if (!running) {
+            running = true;
+            // Inicia un nuevo hilo para actualizar step en segundo plano
+            new Thread(() -> {
+                for (int i = 0; i <= 5; i++) {
+                    try {
+                        step = i; // Actualiza el valor
+                        Thread.sleep(3000); // Espera 3 segundos
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        break;
+                    }
+                }
+                running = false; // Finaliza la ejecución al llegar a 5
+            }).start();
+        }
     }
 }
