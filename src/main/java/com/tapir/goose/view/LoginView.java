1a319d0fa8e2741853a054a21b2b908cf5413530
nigue <anibaldavor@gmail.com>
2025-01-01 23:34:15 -0300

    not merge: js using faces, and not use jsf

---
+++ src/main/java/com/tapir/goose/view/LoginView.java
@@ -0,0 +1,39 @@
+package com.tapir.goose.view;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.Serializable;
+
+@Named("login")
+@RequestScoped
+public class LoginView implements Serializable {
+
+    private static final Logger logger = LogManager.getLogger(LoginView.class);
+
+    private Integer loadStep;
+
+    @PostConstruct
+    public void init() {
+        loadStep = 0;
+    }
+
+    public Integer getLoadStep() {
+        return loadStep;
+    }
+
+    public void setLoadStep(Integer loadStep) {
+        this.loadStep = loadStep;
+    }
+
+    public void execute() throws InterruptedException {
+        loadStep++;
+        Thread.sleep(2000);
+        loadStep++;
+        Thread.sleep(2000);
+    }
+
+}
