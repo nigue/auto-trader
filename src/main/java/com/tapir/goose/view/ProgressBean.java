03409dae5ffe020966d457bd20c26500a1c43a4e
nigue <anibaldavor@gmail.com>
2025-01-02 15:39:59 -0300

    not merge: loading example

---
+++ src/main/java/com/tapir/goose/view/ProgressBean.java
@@ -0,0 +1,27 @@
+package com.tapir.goose.view;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+@Named("indexBean")
+@SessionScoped
+public class ProgressBean implements Serializable {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String action() throws Exception{
+        Thread.sleep(6000);
+        System.out.println(message);
+        return "";
+    }
+}
