d59a5871601c9ffcba917491c8f6f2be6bf0e4cc
nigue <anibaldavor@gmail.com>
2025-01-06 19:49:08 -0300

    feat: add error case

--- src/main/java/com/tapir/goose/view/LoginView.java
+++ src/main/java/com/tapir/goose/view/LoginView.java
@@ -2,6 +2,8 @@ package com.tapir.goose.view;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,34 +16,47 @@ public class LoginView implements Serializable {
 
     private static final Logger logger = LogManager.getLogger(LoginView.class);
 
-    private Integer loadStep;
+    private String message;
+    private Boolean condition;
 
     @PostConstruct
     public void init() {
-        loadStep = 0;
+        logger.info("Init Login");
+        condition = false;
     }
 
-    public Integer getLoadStep() {
-        return loadStep;
+    public String getMessage() {
+        return message;
     }
 
-    public Long getDateView() {
-        return System.currentTimeMillis();
+    public void setMessage(String message) {
+        this.message = message;
     }
 
-    public void execute() throws InterruptedException {
+    public Boolean getCondition() {
+        return condition;
+    }
 
-        logger.info("loadStep: {}", loadStep);
-        loadStep++;
-        Thread.sleep(2000);
-        logger.info("loadStep: {}", loadStep);
-        loadStep++;
-        Thread.sleep(2000);
-        logger.info("loadStep: {}", loadStep);
-        loadStep++;
-        Thread.sleep(2000);
-        logger.info("loadStep: {}", loadStep);
-        loadStep++;
+    public String action() throws InterruptedException {
+        if (!condition) {
+            message = "error from message";
+            logger.error("Invalid credentials");
+            FacesMessage message = new FacesMessage(
+                    FacesMessage.SEVERITY_ERROR,
+                    "Authentication failed error ...",
+                    "error");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, message);
+            return null;
+        }
+        logger.error("step 1: validate key-secret in binance");
+        logger.error("step 1.1: if key-secret are invalid show error");
+        logger.error("step 1.2: if key-secret are invalid delete user (thread)");
+        Thread.sleep(3000);
+        logger.error("step 2: get or create user");
+        Thread.sleep(3000);
+        logger.info("message: {}", message);
+        return "site";
     }
 
 }
