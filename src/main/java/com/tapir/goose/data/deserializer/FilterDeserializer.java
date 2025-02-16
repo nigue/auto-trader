ce8c4e09d5d52b86b79a98f950e812e429f45b37
nigue <anibaldavor@gmail.com>
2025-02-16 11:54:48 -0300

    feat: Deserialize symbol pt6
    Add filter

--- src/main/java/com/tapir/goose/data/deserializer/FilterDeserializer.java
+++ src/main/java/com/tapir/goose/data/deserializer/FilterDeserializer.java
@@ -19,6 +19,19 @@ public class FilterDeserializer implements JsonbDeserializer<FilterDTO> {
         BigDecimal minPrice = BigDecimal.ZERO;
         BigDecimal maxPrice = BigDecimal.ZERO;
         BigDecimal tickSize = BigDecimal.ZERO;
+        BigDecimal minQty = BigDecimal.ZERO;
+        BigDecimal maxQty = BigDecimal.ZERO;
+        BigDecimal stepSize = BigDecimal.ZERO;
+        Integer limit = 0;
+        Integer minTrailingAboveDelta = 0;
+        Integer maxTrailingAboveDelta = 0;
+        Integer minTrailingBelowDelta = 0;
+        Integer maxTrailingBelowDelta = 0;
+        String bidMultiplierUp = "";
+        String bidMultiplierDown = "";
+        String askMultiplierUp = "";
+        String askMultiplierDown = "";
+        Integer avgPriceMins = 0;
         while (parser.hasNext()) {
             JsonParser.Event event = parser.next();
             switch (event) {
@@ -36,6 +49,47 @@ public class FilterDeserializer implements JsonbDeserializer<FilterDTO> {
                     if ("tickSize".equals(key)) {
                         tickSize = new BigDecimal(parser.getString());
                     }
+                    if ("minQty".equals(key)) {
+                        minQty = new BigDecimal(parser.getString());
+                    }
+                    if ("maxQty".equals(key)) {
+                        maxQty = new BigDecimal(parser.getString());
+                    }
+                    if ("stepSize".equals(key)) {
+                        stepSize = new BigDecimal(parser.getString());
+                    }
+                    if ("bidMultiplierUp".equals(key)) {
+                        bidMultiplierUp = parser.getString();
+                    }
+                    if ("bidMultiplierDown".equals(key)) {
+                        bidMultiplierDown = parser.getString();
+                    }
+                    if ("askMultiplierUp".equals(key)) {
+                        askMultiplierUp = parser.getString();
+                    }
+                    if ("askMultiplierDown".equals(key)) {
+                        askMultiplierDown = parser.getString();
+                    }
+                }
+                case VALUE_NUMBER -> {
+                    if ("limit".equals(key)) {
+                        limit = parser.getInt();
+                    }
+                    if ("minTrailingAboveDelta".equals(key)) {
+                        minTrailingAboveDelta = parser.getInt();
+                    }
+                    if ("maxTrailingAboveDelta".equals(key)) {
+                        maxTrailingAboveDelta = parser.getInt();
+                    }
+                    if ("minTrailingBelowDelta".equals(key)) {
+                        minTrailingBelowDelta = parser.getInt();
+                    }
+                    if ("maxTrailingBelowDelta".equals(key)) {
+                        maxTrailingBelowDelta = parser.getInt();
+                    }
+                    if ("avgPriceMins".equals(key)) {
+                        avgPriceMins = parser.getInt();
+                    }
                 }
                 default -> {
                 }
@@ -44,6 +98,19 @@ public class FilterDeserializer implements JsonbDeserializer<FilterDTO> {
         return new FilterDTO(filterType,
                 minPrice,
                 maxPrice,
-                tickSize);
+                tickSize,
+                minQty,
+                maxQty,
+                stepSize,
+                limit,
+                minTrailingAboveDelta,
+                maxTrailingAboveDelta,
+                minTrailingBelowDelta,
+                maxTrailingBelowDelta,
+                bidMultiplierUp,
+                bidMultiplierDown,
+                askMultiplierUp,
+                askMultiplierDown,
+                avgPriceMins);
     }
 }
--- src/main/java/com/tapir/goose/data/dto/FilterDTO.java
+++ src/main/java/com/tapir/goose/data/dto/FilterDTO.java
@@ -6,6 +6,19 @@ public record FilterDTO(
         String filterType,
         BigDecimal minPrice,
         BigDecimal maxPrice,
-        BigDecimal tickSize
+        BigDecimal tickSize,
+        BigDecimal minQty,
+        BigDecimal maxQty,
+        BigDecimal stepSize,
+        Integer limit,
+        Integer minTrailingAboveDelta,
+        Integer maxTrailingAboveDelta,
+        Integer minTrailingBelowDelta,
+        Integer maxTrailingBelowDelta,
+        String bidMultiplierUp,
+        String bidMultiplierDown,
+        String askMultiplierUp,
+        String askMultiplierDown,
+        Integer avgPriceMins
 ) {
 }
