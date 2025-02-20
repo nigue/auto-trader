f144a5f132f925c7cb1c8462c816b2bb9e020a5e
nigue <anibaldavor@gmail.com>
2025-02-20 18:59:39 -0300

    feat: Easy operation

--- src/main/java/com/tapir/goose/service/OptionsService.java
+++ src/main/java/com/tapir/goose/service/OptionsService.java
@@ -8,6 +8,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -15,6 +16,8 @@ import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class OptionsService {
+
+    private static final BigDecimal HUNDRED = BigDecimal.valueOf(100L);
 
     @Inject
     private KlineGateway klineGateway;
@@ -32,25 +35,25 @@ public class OptionsService {
     public List<BinanceVDO> process() {
         return availableSymbols.stream()
                 .map(this::analysisSymbol)
-                .filter(it -> it.red() > 3)
+                .filter(it -> it.red() > 6)
                 .filter(it -> BigDecimal.ZERO.compareTo(it.fall()) > 0)
                 .collect(Collectors.toList());
     }
 
     private BinanceVDO analysisSymbol(String symbol) {
         Interval interval = Interval.FIFTEEN_MINUTES;
-        int limit = 5;
+        int limit = 10;
         List<KlineDTO> klines = klineGateway.findAll(symbol, interval, limit);
         klines.sort(Comparator.comparing(KlineDTO::closeTime));
         Integer red = 0;
         for (KlineDTO kline : klines) {
             if (kline.isRed()) {
                 red++;
-            } else {
-                break;
             }
         }
-        BigDecimal fall = klines.get(0).open().subtract(klines.get(limit - 1).close());
+        BigDecimal fall = HUNDRED.multiply(klines.get(limit - 1).close())
+                .divide(klines.get(limit - 1).close(), RoundingMode.DOWN)
+                .subtract(HUNDRED);
         return new BinanceVDO(symbol,
                 interval.name(),
                 red,
